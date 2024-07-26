06d6fb6695029baf1d4fbe6d773566f8f3300dcf
Victor Oliveira <victor.vdo@outlook.com>
2024-07-26 19:38:28 +0100

    Add new files

---
+++ src/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+mongoose.connect('mongodb://localhost:27017');
+const Product = require('./models/product');
+const Customer = require('./models/customer');
+
+const indexRoutes = require('./routes/index-route');
+const productRoutes = require('./routes/products-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoutes);
+app.use('/products', productRoutes);
+
+module.exports = app;
