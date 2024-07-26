f3ab6675ea378e12a29bb8741924bf76291c7780
Victor Oliveira <37857818+victor-vdo@users.noreply.github.com>
2024-07-26 23:55:57 +0100

    filtering order datas

--- src/repositories/order-repository.js
+++ src/repositories/order-repository.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose');
 const Order = mongoose.model('Order');
 
 exports.get = async(data) => {
-    var res = await Order.find({});
+    var res = await Order
+    .find({}, 'number statusc customer items')
+    .populate('customer', 'name')
+    .populate('items.product', 'title');
     return res;
 }
 
